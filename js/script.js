61630893ef1b3f586d8fb4ed69e84a4cadef5f60
dkbeifus <dkbeifus@gmail.com>
2026-04-19 00:01:47 -0500

    Updated everything

--- js/script.js
+++ js/script.js
@@ -11,14 +11,49 @@ function shuffleItems(textarea) {
     textarea.value = lines.join('\n');
 }
 
-
-async function rollDice(textarea) {
+async function rollDice(textarea, choice_field) {
     textarea.style.backgroundColor = 'purple';
-    textarea.style.color = 'white';
+    textarea.style.color = ' rgb(240, 230, 240)';
     for (let i = 0; i < 50; i++){
-        await sleep(50);
+        await sleep(50); //50 ms pause
         shuffleItems(textarea);
     }
-    textarea.style.backgroundColor = 'white';
-    textarea.style.color = 'black';
+    let lines = textarea.value.split('\n').filter(item => item.trim() !== '');
+    if (lines.length > 0 ){
+        let i = Math.floor(Math.random() * (lines.length));
+        choice_field.value = "The Dice Chose: " + lines[i];
+        textarea.style.backgroundColor = ' rgb(240, 230, 240)';
+        textarea.style.color = 'black';
+    }
+}
+
+async function saveListAsFile(textarea){
+    const text = textarea.value; 
+    const blob = new Blob([text], { type: 'text/plain' });
+    //Both parts of the if statement have been chosen to ensure a saveas dialog appears.
+    if ('showSaveFilePicker' in window) {
+        const fileHandle = await window.showSaveFilePicker({suggestedName: 'RollTheDiceList.txt', types: [{description: 'Text Files', accept: { 'text/plain': ['.txt'] },},]});
+        const writable = await fileHandle.createWritable();
+        await writable.write(blob);
+        await writable.close();
+    } else {
+        //For Firefox and other browsers that do not support showSaveFilePicker()
+        let filename = "RollTheDiceList.txt";
+        const link = document.createElement('a');
+        link.href = URL.createObjectURL(blob);
+        link.download = filename;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+        URL.revokeObjectURL(link.href);
+    }    
+}
+
+async function importListFromFile(textarea, file_input_element){
+    file_input_element.addEventListener('change', function () {
+        let reader = new FileReader(); 
+        reader.onload = function () {textarea.value = reader.result;}
+        reader.readAsText(this.files[0]);
+    })
+    file_input_element.click()
 }
